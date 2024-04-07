32f80684d0b0c0d7572d4f3201b20dda2206cf00
rareHalex <69391092+rareHalex@users.noreply.github.com>
2024-04-07 15:04:43 +0700

    Update exporter_main_v2.py

--- research/object_detection/exporter_main_v2.py
+++ research/object_detection/exporter_main_v2.py
@@ -101,7 +101,7 @@ from absl import flags
 
 import tensorflow.compat.v2 as tf
 from google.protobuf import text_format
-from object_detection import exporter_lib_v2
+import exporter_lib_v2
 from object_detection.protos import pipeline_pb2
 
 tf.enable_v2_behavior()
